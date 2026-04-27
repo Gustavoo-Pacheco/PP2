9868d0260b466f82cec2ecab7cd375f6fc5d291c
Eduardo Lessa <eduardomtl@al.insper.edu.br>
2026-04-27 17:50:42 -0300

    Questao 2

--- funcoes.py
+++ funcoes.py
@@ -7,3 +7,14 @@ def rolar_dados(quantidade):
         dado = random.randint(1, 6)
         dados.append(dado)
     return dados
+
+def guardar_dado(dados_rolados, dados_guardados, indice_dado):
+    dado = dados_rolados[indice_dado]
+    listanova = []
+    for i in range(len(dados_rolados)):
+        if i != indice_dado:
+            listanova.append(dados_rolados[i])
+    dados_rolados = listanova
+    dados_guardados.append(dado)
+    return [dados_rolados, dados_guardados]
+
